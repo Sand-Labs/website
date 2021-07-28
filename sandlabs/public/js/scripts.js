d48985924e4deab80b33771816e36ee2af288b56
Wyatt <wyattowalsh@gmail.com>
2021-07-27 21:49:20 -0700

    good version with working projects section

---
+++ sandlabs/public/js/scripts.js
@@ -0,0 +1,61 @@
+var body = document.querySelector('body')
+var menuTrigger = document.querySelector('#toggle-main-menu-mobile');
+var menuContainer = document.querySelector('#main-menu-mobile');
+
+menuTrigger.onclick = function() {
+    menuContainer.classList.toggle('open');
+    menuTrigger.classList.toggle('is-active')
+    body.classList.toggle('lock-scroll')
+}
+
+$(function(){
+
+  window.sr = ScrollReveal();
+
+  if ($(window).width() < 768) {
+
+  	if ($('.timeline-content').hasClass('js--fadeInLeft')) {
+  		$('.timeline-content').removeClass('js--fadeInLeft').addClass('js--fadeInRight');
+  	}
+
+  	sr.reveal('.js--fadeInRight', {
+	    origin: 'right',
+	    distance: '300px',
+	    easing: 'ease-in-out',
+	    duration: 800,
+	  });
+
+  } else {
+  	
+  	sr.reveal('.js--fadeInLeft', {
+	    origin: 'left',
+	    distance: '300px',
+		  easing: 'ease-in-out',
+	    duration: 800,
+	  });
+
+	  sr.reveal('.js--fadeInRight', {
+	    origin: 'right',
+	    distance: '300px',
+	    easing: 'ease-in-out',
+	    duration: 800,
+	  });
+
+  }
+  
+  sr.reveal('.js--fadeInLeft', {
+	    origin: 'left',
+	    distance: '300px',
+		  easing: 'ease-in-out',
+	    duration: 800,
+	  });
+
+	  sr.reveal('.js--fadeInRight', {
+	    origin: 'right',
+	    distance: '300px',
+	    easing: 'ease-in-out',
+	    duration: 800,
+	  });
+
+
+});
